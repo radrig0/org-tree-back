933799e50d074e27fd1d60820f9327fe495c290f
merokhin <erohin.mihail@gmail.com>
2024-07-10 02:29:17 +0200

    added config for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,31 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { Employee } from './employees/employee.entity';
 import { EmployeesModule } from './employees/employees.module';
+import { readFileSync } from 'fs';
+import 'dotenv/config';
+import * as process from 'node:process';
+import * as path from 'node:path';
+
+const sslCertPath = path.join(__dirname, '..', 'certs', 'ca.pem');
+const sslCert = readFileSync(sslCertPath).toString();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'admin',
-      database: 'app-raise',
+      host: process.env.DBHOST,
+      port: Number(process.env.DBPORT),
+      username: process.env.DBUSER,
+      password: process.env.DBPASSWORD,
+      database: process.env.DBNAME,
       entities: [Employee],
       synchronize: true,
+      logging: true,
+      applicationName: 'org-tree',
+      ssl: {
+        rejectUnauthorized: true,
+        ca: sslCert,
+      },
     }),
     EmployeesModule,
   ],
